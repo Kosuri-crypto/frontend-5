b19f8ef0534e6ff154784df517c98db650740a41
Kosuri-crypto <yura.71rus@yandex.ru>
2023-11-27 01:07:53 +0300

    v.1 + jquery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './index.scss';
+import $ from "jquery";
+
+$(function () {
+    $('.nav-item').click(function () {
+        $('.nav-item--active').toggleClass('nav-item--active');
+        $(this).toggleClass('nav-item--active');
+    });
+});
